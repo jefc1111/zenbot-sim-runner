d19c3777ce098d8eed55674b1569c3b13711a49d
jefc <jefc_uk@hotmail.com>
2021-08-25 21:20:12 +0100

    log

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        \Log::error($this->app->environment());
         if ($this->app->environment('production')) {
             \URL::forceScheme('https');
         }
