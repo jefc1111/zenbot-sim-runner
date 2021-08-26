2ef7495414b038bdbd2020e1304b1722cb585335
jefc <jefc_uk@hotmail.com>
2021-08-26 21:23:54 +0100

    Add note about only Binance

--- resources/views/shared/private_beta_warning.blade.php
+++ resources/views/shared/private_beta_warning.blade.php
@@ -3,8 +3,10 @@
     <br />
     <small>
         This platform is still in rapid & early development. Expect bugs, DB wipes and unexpected / unpredictable behaviour.
-        Do not risk your real money (by live trading) based on the results of this platform. If you do, you do so entirely at your own risk.
-    </small>
+        Do not risk your real money (by live trading) based on the results of this platform. If you do, you do so entirely at your own risk.        
+        <br />
+        <strong>Note:</strong> currently only Binance is enabled. 
+    </small>        
     <br />
     <a target="_blank" href="https://discord.gg/Xa5FQtXt">Discord</a>
 </div> 
