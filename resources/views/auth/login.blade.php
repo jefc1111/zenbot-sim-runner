cad5a1ead875b371da1dac9ca69474fbbf536dc7
jefc <jefc_uk@hotmail.com>
2021-08-26 20:46:14 +0100

    Add login page warning banner

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,10 +3,14 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
+        <div class="col-md-8">        
+            <div class="alert alert-danger" role="alert">
+                <strong>Important Warning - private / unstable beta release</strong></br>
+                This platform is still in rapid & early development. Expect bugs, DB wipes and unexpected / unpredictable behaviour.
+                Do not risk your real money (by live trading) based on the results of this platform. If you do, you do so entirely at your own risk.
+            </div> 
+            <div class="card">            
+                <div class="card-header">{{ __('Login') }}</div>                
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
