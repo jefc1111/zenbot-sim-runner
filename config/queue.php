f62566371ea85617a120f22d21a13b9006c8650a
jefc <jefc_uk@hotmail.com>
2021-08-30 10:03:16 +0100

    Up limit to 4 hours

--- config/queue.php
+++ config/queue.php
@@ -63,7 +63,7 @@ return [
             'driver' => 'redis',
             'connection' => 'default',
             'queue' => env('REDIS_QUEUE', 'default'),
-            'retry_after' => 900,
+            'retry_after' => 14400,
             'block_for' => null,
         ],
 
