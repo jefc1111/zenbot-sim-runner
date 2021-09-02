dbdb6c70bd2695792b9679aced507219e39b4905
jefc <jefc_uk@hotmail.com>
2021-09-02 20:28:50 +0100

    clear config at start

--- docker/start.sh
+++ docker/start.sh
@@ -7,7 +7,7 @@ env=${APP_ENV:-production}
 
 if [ "$env" != "local" ]; then
     echo "Caching configuration..."
-    (cd /var/www/html && php artisan config:cache && php artisan route:cache && php artisan view:cache)
+    (cd /var/www/html && php artisan config:cache && php artisan route:cache && php artisan view:cache && php artisan config:clear)
 fi
 
 if [ "$role" = "app" ]; then
