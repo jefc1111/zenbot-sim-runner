5b8fd5a8c6ef15936fb4841195c243999bbc72a9
jefc <jefc_uk@hotmail.com>
2021-09-29 20:43:56 +0100

    Fix bug with active tab not showing

--- resources/views/sim_run_batches/show/tab_header_item.blade.php
+++ resources/views/sim_run_batches/show/tab_header_item.blade.php
@@ -1,5 +1,10 @@
 <li class="nav-item">
-    <a id="{{ $id }}-tab" data-toggle="tab" class="nav-link" href="#{{ $id }}">
+    <a 
+        id="{{ $id }}-tab" 
+        data-toggle="tab" 
+        class="nav-link {{ isset($active) && $active == true ? 'active' : null }}" 
+        href="#{{ $id }}"
+    >
         {{ $label }}
     </a>
 </li>
