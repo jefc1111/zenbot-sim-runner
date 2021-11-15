262c27a12ddd73afda6d39b228719a05917d690d
jefc <jefc_uk@hotmail.com>
2021-11-15 22:01:16 +0000

    Add sim time information text

--- resources/views/shop/main.blade.php
+++ resources/views/shop/main.blade.php
@@ -26,6 +26,11 @@
         <tfoot>
         </tfoot>
     </table>
+    <div class="alert alert-warning" role="alert">
+        <h4 class="alert-heading">Important information about sim time bundles</h4>
+        <p>Simulations run in parallel, and sime time is used up per simulation, not for the overall run time of the batch.</p>
+        <p>Example: A batch has 96 simulations, each lasts 15 minutes. This will consume 24 hours of sim time.</p>
+    </div>    
     <h4>Previous orders</h4>
     <table class="table table-sm table-bordered">
         <thead>
