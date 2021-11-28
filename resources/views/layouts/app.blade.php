d392808a1411c1ee8f2906e948173e793f0f84f7
jefc <jefc_uk@hotmail.com>
2021-11-28 09:35:26 +0000

    Add logos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,13 +18,16 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">    
+
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Zenbot Sim Runner') }}
+                    <img height="22" class="" src="{{ asset('images/zsr-logo-no-text.png') }}" alt="ZSR logo">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
