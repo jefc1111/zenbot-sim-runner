5483ed01a99d39503ff3f96c574c542bd2166065
jefc <jefc_uk@hotmail.com>
2021-12-04 13:29:24 +0000

    Add page for users waiting for approval

---
+++ resources/views/auth/awaiting_approval.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Awaiting approval') }}</div>
+
+                    <div class="card-body">
+                        An administrator needs to approve your account before you can use the app. 
+                        <br><br>
+                        <a href="https://discord.com/channels/880347822854115328/880347822854115331" target="_blank">Contact me on Discord</a>                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
