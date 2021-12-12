a162c10362b7704134c4d9a111ed2fb2477f3bc1
jefc <jefc_uk@hotmail.com>
2021-12-12 20:20:33 +0000

    Fix btn css

--- resources/views/sim_run_batches/show/admin_btn.blade.php
+++ resources/views/sim_run_batches/show/admin_btn.blade.php
@@ -1,11 +1,11 @@
 <div class="row">    
     @if ($method === 'get')
-    <a onClick="return confirm('Are you sure you want to {{ strtolower($btn_text) }} this batch?')" style="margin: 3px; " type="button" class="btn btn-block btn-secondary col-md-3" href="{{ $href }}">
+    <a onClick="return confirm('Are you sure you want to {{ strtolower($btn_text) }} this batch?')" style="margin: 3px; " type="button" class="btn btn-secondary" href="{{ $href }}">
         {{ $btn_text }}
     </a>
     @else
     <form action="{{ $href }}" method="post">
-        <input onClick="return confirm('Are you sure you want to {{ strtolower($btn_text) }} this batch?')" style="margin: 3px; " class="btn btn-block btn-secondary col-md-3" type="submit" value="Delete" />
+        <input onClick="return confirm('Are you sure you want to {{ strtolower($btn_text) }} this batch?')" style="margin: 3px; " class="btn btn-secondary" type="submit" value="Delete" />
         @method($method)
         @csrf
     </form>
