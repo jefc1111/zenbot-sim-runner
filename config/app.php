c905c487c71e9265ca840c5233e959c91faedee1
jefc <jefc_uk@hotmail.com>
2021-12-18 20:26:16 +0000

    Add AWS SDK

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Aws\Laravel\AwsServiceProvider::class,
 
     ],
 
@@ -227,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'AWS' => Aws\Laravel\AwsFacade::class,
     ],
 
 ];
