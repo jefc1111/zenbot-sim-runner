9afc84c624556a44460e3e56b4d46ccc2e95955a
jefc <jefc_uk@hotmail.com>
2021-12-22 14:22:10 +0000

    Add the actual cluster spinning logic

--- app/Console/Commands/check_worker_clusters.php
+++ app/Console/Commands/check_worker_clusters.php
@@ -43,32 +43,58 @@ class check_worker_clusters extends Command
 
         $target_var = 'desiredCount';
 
-        $ecs_service_names = [
-            config('aws-zsr.sim_runner_service_name'),
-            config('aws-zsr.backfill_service_name')
+        $ecs_queue_service_name_map = [
+            'sim' => config('aws-zsr.sim_runner_service_name'),
+            'backfill' => config('aws-zsr.backfill_service_name')
         ];
 
         $res = $aws_ecs->describeServices([
             'cluster' => $ecs_cluster_name,
-            'services' => $ecs_service_names
+            'services' => array_values($ecs_queue_service_name_map)
         ]);
 
-        foreach ($ecs_service_names as $ecs_service_name) {
+        foreach ($ecs_queue_service_name_map as $queue_name => $ecs_service_name) {
             if ($res && $res->hasKey('services') && is_array($res->get('services'))) {
+                $queue_size = \Queue::size($queue_name);
+            
+                \Log::error("$queue_name queue size $queue_size");
+
                 $$target_var = $res->search("services.[?serviceName==`$ecs_service_name`].$target_var | [0]");
 
                 \Log::error("$target_var for service '$ecs_service_name' is {$$target_var}");
+
+                if ($queue_size === 0 && $$target_var > 0) {
+                    \Log::error("Spinning down service '$ecs_service_name'");
+
+                    $this->spin_down_cluster($aws_ecs, $ecs_service_name);                    
+                } else if ($queue_size > 0 && $$target_var === 0) {
+                    \Log::error("Spinning up service '$ecs_service_name'");
+
+                    $this->spin_up_cluster($aws_ecs, $ecs_service_name);
+                } else {
+                    \Log::error("No cluster spinning action required for service '$ecs_service_name'");
+                }
             } else {
                 \Log::error("Could not retrieve '$target_var' from AWS describeServices response for service '$ecs_service_name'.");
             }
         }
+    }
 
-        foreach (['backfill', 'sim'] as $queue_name) {
-            $queue_size = \Queue::size($queue_name);
-            
-            \Log::error("$queue_name queue size $queue_size");
+    private function spin_down_cluster($client, string $service_name)
+    {
+        $res = $client->updateService([
+            'cluster' => config('aws-zsr.cluster_name'),
+            'desiredCount' => 0,
+            'service' => $service_name,
+        ]);
+    }
 
-            // if $queue_size === 0 && desiredCount > 1 set desiredCount to 0  
-        } 
+    private function spin_up_cluster($client, string $service_name)
+    {
+        $result = $client->updateService([
+            'cluster' => config('aws-zsr.cluster_name'),
+            'desiredCount' => 1,
+            'service' => $service_name,
+        ]);
     }
 }
