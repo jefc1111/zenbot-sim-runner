26278343269e90a518b8aaff4c812967a0248ac3
jefc <jefc_uk@hotmail.com>
2022-02-21 15:34:29 +0000

    Fix content in cell

--- resources/views/table_components/good_bad_cell.blade.php
+++ resources/views/table_components/good_bad_cell.blade.php
@@ -3,5 +3,5 @@
     class="text-{{ $value < 0 ? 'danger' : 'success' }}"
     @endif
 >
-    {{ $bot->latest_snapshot ? ($bot->latest_snapshot->profit - $bot->latest_snapshot->buy_hold_profit).'%' : null }}
+    {{ $content }}
 </td>
